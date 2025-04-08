891164879c590234fe978deeb486bf3210240b22
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-08 20:33:05 +0600

    add bootstrap

--- Praktish/javascript/main.js
+++ Praktish/javascript/main.js
@@ -1,4 +1,19 @@
+// button clicking
+function handelClickBtn() {
+   let Status = document.getElementById('Tst');
+   Status.innerHTML=`
+   <p class="js_Text">Howrah</p>
+   `
+   alert("card is add")
+}
 
-
+// input field
+document.getElementById('comment').addEventListener('click',function(){
+   let input = document.getElementById('InText');
+   let inputText = input.value;
+   let comment = document.getElementById('commentText');
+   comment.innerText = inputText;
+   input.value = ''
+})
 
 
