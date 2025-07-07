18a9a46393c7df43b7b4576aa8eabff04128f633
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-07 12:22:08 +0600

    ok

---
+++ Praktish_JS/start/first.js
@@ -0,0 +1,42 @@
+// node first.js
+
+// 1
+console.log("Hello World");
+
+
+// 2
+function Name(){
+    console.log("function world");
+    
+};
+Name();
+
+
+// 3
+function ValueF() {
+    return 2;
+};
+
+let valueIs = ValueF();
+
+console.log(valueIs);
+
+// 4
+let ok;
+
+function ValueFunction(a){
+    return (a+a)
+};
+
+ok = ValueFunction(2);
+
+console.log(ok);
+
+// 5
+if (ok===2){
+ return 'ok is true'
+}
+
+
+
+
---
+++ Praktish_JS/start/praktis.js
@@ -0,0 +1,5 @@
+// node praktis.js
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
