71cc4235f50a51efd097c1710abdaaff0c728cb3
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-09 18:56:12 +0600

    the sort system is lern

--- React/script/javascript/sort/sort.js
+++ React/script/javascript/sort/sort.js
@@ -1,18 +1,69 @@
 // node sort.js
 
 let data = [
-    {id:3, name:"D" , isStudent:true, date:"2023-04-01"},
-    {id:1, name:"B" , isStudent:true, date:"2023-06-01"},
-    {id:5, name:"F" , isStudent:true, date:"2023-02-01"},
-    {id:0, name:"A" , isStudent:true, date:"2023-07-01"},
-    {id:2, name:"C" , isStudent:true, date:"2023-05-01"},
-    {id:4, name:"E" , isStudent:true, date:"2023-03-01"},
+    {id:3, name:"D" , isStudent:true, date:"2023-04-01" },
+    {id:1, name:"B" , isStudent:false, date:"2023-06-01"},
+    {id:5, name:"F" , isStudent:true, date:"2023-02-01" },
+    {id:0, name:"A" , isStudent:true, date:"2023-07-01" },
+    {id:2, name:"C" , isStudent:false, date:"2023-05-01"},
+    {id:4, name:"E" , isStudent:true, date:"2023-03-01" },
 ]
 
-data = data.sort((a,b) =>{
-    if (a.id < b.id) {
-        return -1
-    }
-})
 
-console.log(data);
+// sort by id
+function SortById() {
+    data = data.sort((a,b) =>{
+        if (a.id < b.id) {
+            return -1
+        }
+    })
+    console.log(data);
+}
+
+SortById()
+
+// sort by id
+function SortByIds() {
+    data = data.sort((a, b) => a.id - b.id);
+    console.log(data);
+}
+
+SortByIds()
+
+// sort by name
+function SortByName() {
+    data = data.sort((a,b) =>{
+        if (a.name < b.name) {
+            return -1
+        }
+    })
+    console.log(data);
+}
+
+// SortByName()
+
+// sort by Student valuation
+function SortByNIsStudent() {
+    data = data.sort((a,b) =>{
+        if (!a.isStudent < !b.isStudent) {
+            return -1
+        }
+    })
+    console.log(data);
+}
+
+// SortByNIsStudent()
+
+// sort by date
+function SortByNDate() {
+    data = data.sort((a,b) =>{
+        let date1 = new Date(a.date) 
+        let date2 = new Date(b.date) 
+        if (date1 < date2) {
+            return -1
+        }
+    })
+    console.log(data);
+}
+
+SortByNDate()
