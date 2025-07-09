ace0a44a7e3fec7564fe9f38abbea53ea864b4cf
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-09 17:57:47 +0600

    update

---
+++ React/script/javascript/sort/sort.js
@@ -0,0 +1,18 @@
+// node sort.js
+
+let data = [
+    {id:3, name:"D" , isStudent:true, date:"2023-04-01"},
+    {id:1, name:"B" , isStudent:true, date:"2023-06-01"},
+    {id:5, name:"F" , isStudent:true, date:"2023-02-01"},
+    {id:0, name:"A" , isStudent:true, date:"2023-07-01"},
+    {id:2, name:"C" , isStudent:true, date:"2023-05-01"},
+    {id:4, name:"E" , isStudent:true, date:"2023-03-01"},
+]
+
+data = data.sort((a,b) =>{
+    if (a.id < b.id) {
+        return -1
+    }
+})
+
+console.log(data);
